8533e0264e6355bee94143812ed0fa429916355a
unknown <bjklayneabordo.fsd@gmail.com>
2024-07-27 15:23:19 +0800

    scanning empty file

---
+++ app/main.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!", file=sys.stderr)
+
+    # Check if the correct number of arguments have been passed.
+    # If not, print usage instructions and exit the program.
+    if len(sys.argv) < 3:
+        print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
+        exit(1)
+
+    # Assign the first argument (after script name) to 'command'.
+    # This should be the operation to perform, e.g., "tokenize".
+    command = sys.argv[1]
+
+    # Assign the second argument to 'filename', which is expected to be the file to process.
+    filename = sys.argv[2]
+
+    # Check if the specified command matches the expected operation ("tokenize").
+    # If not, print an error message and exit the program.
+    if command != "tokenize":
+        print(f"Unknown command: {command}", file=sys.stderr)
+        exit(1)
+
+    with open(filename) as file:
+        file_contents = file.read()
+
+    # Open the specified file and read its contents into 'file_contents'.
+    if file_contents:
+        raise NotImplementedError("Scanner not implemented")
+    else:
+        # If the file is empty, print a placeholder message indicating EOF (End Of File).
+        # This line should be removed once the scanner is fully implemented.
+        print("EOF  null")
+
+
+if __name__ == "__main__":
+    main()
